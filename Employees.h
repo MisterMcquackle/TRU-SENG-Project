3ce3e18146d6bce1e29fc92300374b17439f4bd0
kwan691 <klcound@gmail.com>
2025-03-17 13:55:05 -0700

    Update Employees.h

--- Employees.h
+++ Employees.h
@@ -6,7 +6,6 @@
 #include <algorithm>
 using namespace std;
 
-int size;
 vector<string> Employees;
 vector<shared_ptr<Employee>> E;
 
@@ -17,21 +16,35 @@ void selectEmployee(string name){
         if (nom->name == name){
             nom->displayInfo();
             found = true;
-            break;
         }
     }
     if (!found){
-        cout << "That person is not an employee" << endl;
+        cout << name << " is not an employee" << endl;
+    }
+}
+
+void scheduleSet(string name){
+    bool found = false;
+    for (const auto& nom : E){
+        if (nom->name == name){
+            nom->setschedule(name);
+            found = true;
+        }
+    }
+    if (!found){
+        cout << name << " is not an employee" << endl;
     }
 }
 
 void hireEmployee(string name, int pos, int wage){
+    cout << name << " has been hired" << endl;
     for (int i = 0; i < Employees.size(); i++){  
         if (Employees[i].empty()){
             Employees[i] = name;
+            E.push_back(make_shared<Employee>(name, pos, wage));
             return;
         }
-    } 
+    }
     E.push_back(make_shared<Employee>(name, pos, wage));
     Employees.push_back(name);
 }
@@ -43,15 +56,15 @@ void fireEmployee(string name){
        }
     }
 
-    auto it = remove_if(E.begin(), E.end(), [&](const shared_ptr<Employee>& emp) {
-            return emp->name == name;
-        });
+    auto it = remove_if(E.begin(), E.end(), [&](const shared_ptr<Employee>& emp){
+        return emp->name == name;
+    });
 
     if (it != E.end()) {
         E.erase(it, E.end());
-        cout << "Employee " << name << " has been removed." << endl;
-    } else {
-        cout << "That person is not an employee." << endl;
+        cout << name << " has been fired" << endl;
+    } 
+    else {
+        cout << name << " is not an employee" << endl;
     }
 }
-
