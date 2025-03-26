70f1579b8ae6799d9d6a5ddec55f35ee861fa252
MisterMcquackle <Davis.Klohn@gmail.com>
2025-03-26 00:32:36 -0700

    -Update For Clarity And Cohesion-

--- PackageMenu.h
+++ PackageMenu.h
@@ -1,12 +1,26 @@
+// ===============================================
+// TANK'S Distribution System
+// Package Menu Interface
+// ===============================================
+
 #ifndef PACKAGEMENU_H
 #define PACKAGEMENU_H
 
 #include "Menu.h"
-#include <iostream>
+#include "Package.h"
+#include <vector>
 
 class PackageMenu : public Menu {
+private:
+    std::vector<Package> packages;
+
 public:
     void display() override;
+
+private:
+    void addPackage();
+    void viewPackages();
+    void updatePackageStatus();
 };
 
-#endif // PACKAGEMENU_H
+#endif
