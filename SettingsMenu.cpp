0344073fbab21edb2665fbb82f51944ced423c97
MisterMcquackle <Davis.Klohn@gmail.com>
2025-03-28 01:10:24 -0700

    -STD BE GONE!-

--- SettingsMenu.cpp
+++ SettingsMenu.cpp
@@ -6,24 +6,26 @@
 #include "SettingsMenu.h"
 #include <iostream>
 
+using namespace std;
+
 void SettingsMenu::display() {
     int choice;
     do {
-        std::cout << "\n\033[1;31m--- System Settings ---\033[0m\n"
-                  << "1. Change Console Theme (Placeholder)\n"
-                  << "2. Back to Main Menu\n"
-                  << "------------------------\n"
-                  << "Enter your choice: ";
-        std::cin >> choice;
+        cout << "\n\033[1;31m--- System Settings ---\033[0m\n"
+             << "1. Change Console Theme (Placeholder)\n"
+             << "2. Back to Main Menu\n"
+             << "------------------------\n"
+             << "Enter your choice: ";
+        cin >> choice;
 
         switch (choice) {
             case 1: changeTheme(); break;
             case 2: break;
-            default: std::cout << "\033[1;31mInvalid choice!\033[0m\n";
+            default: cout << "\033[1;31mInvalid choice!\033[0m\n";
         }
     } while (choice != 2);
 }
 
 void SettingsMenu::changeTheme() {
-    std::cout << "\033[1;33m[Theme] Console theme set to default.\033[0m\n";
+    cout << "\033[1;33m[Theme] Console theme set to default.\033[0m\n";
 }
